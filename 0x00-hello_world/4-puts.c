c694c9c6fd745bdb861a206827e96e1417634e17
Your Name <zulafamaalim@gmail.com>
2023-02-16 07:36:26 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line."
- * return 0.
+ * puzzle, followed by a new line.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -12,5 +12,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
