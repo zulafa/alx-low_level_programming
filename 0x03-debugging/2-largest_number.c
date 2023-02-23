2986a869811e4ba05e166839699cf713a3138222
Your Name <zulafamaalim@gmail.com>
2023-02-22 23:15:16 -0800

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,10 +16,14 @@ if (a > b && b > c)
 {
 largest = a;
 }
+else if (a > c && c > b)
+	largest = a;
+else if (b > c && c > a)
+	largest = b;
 else if (b > a && a > c)
-{
 largest = b;
-}
+else if (c > a && a > b)
+	largest = c;
 else
 {
 largest = c;
