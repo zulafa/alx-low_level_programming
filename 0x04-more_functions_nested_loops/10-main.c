a6fe54cdb5c0349716d40517e17e2dbcd4d5405e
Your Name <zulafamaalim@gmail.com>
2023-02-23 16:17:45 -0800

    10-main.c

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -5,14 +5,11 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
-
 {
-	print_triangle(2);
-	print_triangle(10);
-	print_triangle(1);
-	print_triangle(0);
-	return (0);
-
+print_triangle(2);
+print_triangle(10);
+print_triangle(1);
+print_triangle(0);
+return (0);
 }
--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,25 +9,25 @@
 void print_triangle(int size)
 
 {
-	int x, y, z;
+int x, y, z;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (x = 0; x < size; x++)
-		{
-			for (y = size - x; y > 1; y--)
-			{
-				_putchar(32);
-			}
-			for (z = 0; z <= x; z++)
-			{
-				_putchar(35);
-			}
-			_puchar('\n');
-		}
-	}
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = size - x; y > 1; y--)
+{
+_putchar(32);
+}
+for (z = 0; z <= x; z++)
+{
+_putchar(35);
+}
+_puchar('\n');
+}
+}
 }
